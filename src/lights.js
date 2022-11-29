415eb817ae35175b3d8cc5b22a365ce5b2c43395
Tamer Sali <tamerrsali@gmail.com>
2022-11-29 16:18:50 +0200

    use different lights for better quality

--- src/lights.js
+++ src/lights.js
@@ -1,9 +1,11 @@
-import { DirectionalLight, PointLight } from "three";
+import { DirectionalLight, PointLight, HemisphereLight } from "three";
 
-const directionalLight = new DirectionalLight("#FFF", 10);
-const pointLightRight = new PointLight("#FFF", 5);
-const pointLightLeft = new PointLight("#FFF", 5);
-pointLightRight.position.set(3,5,-2)
-pointLightLeft.position.set(-3,5,2)
+const directionalLight = new DirectionalLight(0xffeeb1, 10);
+const pointLightRight = new PointLight(0xffeeb1, 2);
+const pointLightLeft = new PointLight(0xffeeb1, 2);
+const hemisphereLight = new HemisphereLight(0xffeeb1, 0x080820, 5);
+directionalLight.position.set(0, 3, 0)
+pointLightRight.position.set(0.5, 3, 2);
+pointLightLeft.position.set(-0.5, 3, -2);
 
-export const lights = [directionalLight, pointLightRight, pointLightLeft];
+export const lights = [hemisphereLight, directionalLight, pointLightLeft, pointLightRight];
