5f88a962aef4672d9a00f280a7ee12c490cc51c4
Tamer Sali <tamerrsali@gmail.com>
2023-04-17 23:37:26 +0300

    update the model material

--- src/models.js
+++ src/models.js
@@ -3,22 +3,24 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 const loader = new GLTFLoader();
 
 const models = [
+
 	{
 		name: "Nissan 350z",
-		asset_url: "./assets/n_350z.glb",
+		asset_url: "n_350z.glb",
 		scale: [1.6, 1.6, 1.6],
 		position: [0, 0.25, 0],
 	},
 	{
 		name: "Skyline R34",
-		asset_url: "./assets/s_r34.glb",
-		scale: [1.4, 1.45, 1.6],
-		position: [0, 0.5, 0],
+		asset_url: "s_r34_2.glb",
+		scale: [2.2, 2.1, 2.1],
+		position: [-4, -0.4, -1.4],
 	},
+
 	{
 		name: "BMW M3",
-		asset_url: "./assets/b_m3.glb",
-		scale: [1.5, 1.5, 1.6],
+		asset_url: "b_m3.glb",
+		scale: [1.6, 1.5, 1.6],
 		position: [0, 0.25, -0.1],
 	},
 ];
@@ -29,6 +31,7 @@ const toggleSceneObjects = (scene, name) => {
 		}
 	});
 };
+
 const render3dModel = (index, scene) => {
 	if (models[index] === undefined) return;
 	const { name, asset_url, scale, position } = models[index];
@@ -40,14 +43,30 @@ const render3dModel = (index, scene) => {
 	}
 
 	loader.load(asset_url, function (gltf) {
+		console.log(gltf)
 		const model = gltf.scene.children[0];
 		model.scale.set(...scale);
 		model.position.set(...position);
 		model.name = name;
-		model.castShadow = true;
 		scene.add(model);
 		toggleSceneObjects(scene, model.name);
+		updateAllMaterials(scene)
 	});
 };
+
+
+/**
+ * Update all materials
+ */
+const updateAllMaterials = (scene) => {
+	scene.traverse((child) => {
+		if (child?.material) {
+			child.material.envMapIntensity = 5
+			child.material.needsUpdate = true
+			child.castShadow = true
+			child.receiveShadow = true
+		}
+	})
+}
 
 export { models, render3dModel };
