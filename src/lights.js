2c35f6a0d2cfab6158742f84b583d73a145b9a0f
Tamer Sali <tamerrsali@gmail.com>
2023-04-17 23:36:22 +0300

    enable light shadow

--- src/lights.js
+++ src/lights.js
@@ -2,6 +2,12 @@ import { DirectionalLight, HemisphereLight } from "three";
 
 const directionalLight = new DirectionalLight(0xffffff, 15);
 const hemisphereLight = new HemisphereLight(0xffeeb1, 0x080820, 5);
-directionalLight.position.set(0, 3.5, 0)
+
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.normalBias = 0.05
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.far = 5;
+directionalLight.position.set(0, 7, 0)
 
 export const lights = [hemisphereLight, directionalLight];
